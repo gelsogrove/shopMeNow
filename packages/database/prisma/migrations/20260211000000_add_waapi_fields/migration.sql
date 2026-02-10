366df07ca6156cf920e9f290bc36235ba30f6835
Andrea Gelsomino <gelsogrove@gmail.com>
2026-02-11 00:07:34 +0100

    feat: add WAAPI fields to Workspace table in migration

---
+++ packages/database/prisma/migrations/20260211000000_add_waapi_fields/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "Workspace" ADD COLUMN     "waapiInstanceId" TEXT,
+ADD COLUMN     "waapiInstanceStatus" TEXT,
+ADD COLUMN     "waapiPhoneNumber" TEXT,
+ADD COLUMN     "waapiPhoneName" TEXT,
+ADD COLUMN     "waapiWebhookUrl" TEXT;
